c6602aa716fb2c94ef7b395f574d48a5b44fa683
Petr E. Antonov <antpeter@gmail.com>
2019-05-13 10:17:38 +0300

    Adding funcs for image downloading

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import os
+import pathlib
+import requests
+
+
+IMAGE_FOLDER = 'images'
+
+logging.basicConfig(
+    format='%(levelname)-8s [%(asctime)s] %(message)s',
+    level=logging.DEBUG,
+    filename='log.log'
+)
+
+
+def get_image(url):
+    pathlib.Path(
+        os.path.join(IMAGE_FOLDER)
+    ).mkdir(parents=True, exist_ok=True)
+    filename = url.split("/")[-1]
+    response = requests.get(url)
+    logging.info(f'Getting image: {os.path.join(IMAGE_FOLDER, filename)}')
+    with open(os.path.join(IMAGE_FOLDER, filename), 'wb') as file:
+        file.write(response.content)
+
+
+def get_image_url(image_num):
+    url = f"http://xkcd.com/{image_num}/info.0.json"
+
+    response = requests.get(url)
+    if response.ok:
+        image_url = response.json()['img']
+        return image_url
+
+
+if __name__ == '__main__':
+    get_image(get_image_url(614))
